ee8b3054d76c45c5cb81668dcd9b2519364284e9
Jack Huddlestone <jack.huddlestone@littlefish.co.uk>
2024-12-05 21:38:01 +0000

    Dynamically find the subnet with the Microsoft.App/environments delegation

--- modules/virtual_network/outputs.tf
+++ modules/virtual_network/outputs.tf
@@ -1,4 +1,4 @@
 output "app_subnet_id" {
-  description = "The ID of the subnet for the application."
-  value       = azurerm_subnet.subnet["snet-app"].id
+  description = "The ID of the subnet with container app delegation."
+  value       = tolist([for subnet in azurerm_subnet.subnet : subnet.id if any(subnet.delegation[*].service_delegation == "Microsoft.App/environments")])[0]
 }
